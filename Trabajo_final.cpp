2e34a14972f6b060747097584daa7bda2b12cc3a
csalan137 <144847388+csalan137@users.noreply.github.com>
2023-11-26 11:36:48 -0600

    Fin
    Se finaliza el trabajo final, corriegiendo salidas y un par d bugs, hay mucho q trabajar, pero cubre los requisitos minimos del trabajo final

--- Trabajo_final.cpp
+++ Trabajo_final.cpp
@@ -1,12 +1,18 @@
 #include<iostream>
 #include<string>
+#include <cstdlib>
+
 using namespace std;
 
 int main (){
-	int opc = 0;
-	string nombreUsuario = "A";
-	bool salida = 0;
+	int opc = 0;//declaracion de variables
+	int x;
+	string nombre_usuario = "A";
+	int salida = 0;
+	int salida_menu = 0;
+	
 	do{
+		
 		system("cls");
 		cout<<"\n\t\tParque Barranca de Huentita"<<endl<<endl;
 		cout<<"1"<<endl;
@@ -17,9 +23,10 @@ int main (){
 		switch(opc){
 			
 			case 1:
+				x = 100;
 				cout<<"\n\tIngresa tu nombre: ";
 				cin.ignore();
-				getline(cin, nombreUsuario);
+				getline(cin, nombre_usuario);
 				system("cls");
 				cout<<"\n\n\tParque Barranca de Huentita"<<endl;
 				cout<<"\n\tRuidos de perro rascando la puerta!"<<endl;
@@ -43,14 +50,15 @@ int main (){
 				cout<<"\n\tEros: Ladridos mas fuertes y agresivos"<<endl;
 				cout<<"\n\tPersona masculina2: ?Callen a ese wuey!"<<endl;
 				cout<<"\n\tEros: Rascando y chillando"<<endl<<endl;
-				cout<<nombreUsuario<<": Habro los ojos y decido levantarme para averiguar que quiere Eros"<<endl;
+				cout<<nombre_usuario<<": Habro los ojos y decido levantarme para averiguar que quiere Eros"<<endl;
 				cout<<"\n\tMe pongo al pie de la cama y decido...\n"<<endl;
 				system("pause");				
 				system("cls");
 				
 				do{
-					cout<<"2"<<endl;
-					cout<<"\n\t1. Cerrar los ojos y volver a dormir\n\t2. Silenciar a mi perro?\n\t3. Buscar mis botas en el ropero?\n\t4. Salir del juego"<<endl;//empieza la segunda interaccion del usuario
+					cout<<"2"<<endl;//segunda interaccion del usuario
+					cout<<"\n\t1. Cerrar los ojos y volver a dormir\n\t2. Silenciar a mi perro?\n\t3. Buscar mis botas en el ropero?\n\t4. Salir al menu principal"<<endl;//empieza la segunda interaccion del usuario
+					cout<<"\n\tPuntos: "<<x<<endl;
 					cout<<"\n\tIngrese la opcion: ";
 					cin>>opc;
 					system("cls");
@@ -79,14 +87,16 @@ int main (){
 							cout<<"\n\tEros: Ladridos mas fuertes y agresivos"<<endl;
 							cout<<"\n\tPersona masculina2: ?Callen a ese wuey!"<<endl;
 							cout<<"\n\tEros: Rascando y chillando"<<endl<<endl;
-							cout<<nombreUsuario<<": Habro los ojos y decido levantarme para averiguar que quiere Eros"<<endl;
+							cout<<nombre_usuario<<": Habro los ojos y decido levantarme para averiguar que quiere Eros"<<endl;
 							cout<<"\n\tMe pongo al pie de la cama y decido...\n"<<endl;
+							cout<<"\n\t-10 Puntos\n"<<endl;
+							x = x - 10;
 							system("pause");				
 							system("cls");
 							break;
 												
 						case 2:
-							cout<<nombreUsuario<<": Silencio Eros...\n"<<endl;
+							cout<<nombre_usuario<<": Silencio Eros...\n"<<endl;
 							system("pause");				
 							system("cls");
 							cout<<"\n\tRuidos de perro rascando la puerta!"<<endl;
@@ -110,8 +120,10 @@ int main (){
 							cout<<"\n\tEros: Ladridos mas fuertes y agresivos"<<endl;
 							cout<<"\n\tPersona masculina2: ?Callen a ese wuey!"<<endl;
 							cout<<"\n\tEros: Rascando y chillando"<<endl<<endl;
-							cout<<nombreUsuario<<": Habro los ojos y decido levantarme para averiguar que quiere Eros"<<endl;
+							cout<<nombre_usuario<<": Habro los ojos y decido levantarme para averiguar que quiere Eros"<<endl;
 							cout<<"\n\tMe pongo al pie de la cama y decido...\n"<<endl;
+							cout<<"\n\t-10 Puntos\n"<<endl;
+							x = x - 10;
 							system("pause");				
 							system("cls");
 							break;
@@ -124,9 +136,11 @@ int main (){
 							cout<<"\n\tLlego al borde del barranco y entre la neblina logro observar a una persona corriendo,\n\tadentrandose en el bosque, decido bajar del acantilado y encuentro a Eros lastimado.\n"<<endl;
 							system("pause");				
 							system("cls");
+							
 							do{
-								cout<<"3"<<endl;
-								cout<<"\n\t1. LLeno de rabia y frustracion persigo a la persona que vi desde el principio.\n\t2. Me quedo con Eros abrazandolo y llorando en silencio.\n\t3. Intento auxiliarlo ya que esta sangrando.\n\t4. Salir del juego.\n"<<endl;
+								cout<<"3"<<endl;//tercer interaccion del usuario
+								cout<<"\n\t1. LLeno de rabia y frustracion persigo a la persona que vi desde el principio.\n\t2. Me quedo con Eros abrazandolo y llorando en silencio.\n\t3. Intento auxiliarlo ya que esta sangrando.\n\t4. Salir al menu principal\n"<<endl;
+								cout<<"\n\tPuntos: "<<x<<endl;
 								cout<<"\n\tIngrese la opcion: ";
 								cin>>opc;				
 								system("cls");
@@ -141,8 +155,9 @@ int main (){
 										system("cls");
 										
 										do{
-											cout<<"4"<<endl;
-											cout<<"\n\t1. Me quedo congelado sin hacer nada.\n\t2. Le pregunto quien es.\n\t3. Bajo de donde subi.\n\t4. Salir del juego"<<endl;
+											cout<<"4"<<endl;//cuarta interaccion del usuario
+											cout<<"\n\t1. Me quedo congelado sin hacer nada.\n\t2. Le pregunto quien es.\n\t3. Bajo de donde subi.\n\t4. Salir al menu principal"<<endl;
+											cout<<"\n\tPuntos: "<<x<<endl;
 											cout<<"\n\tIngrese la opcion: ";
 											cin>>opc;				
 											system("cls");
@@ -150,7 +165,9 @@ int main (){
 											switch(opc){
 												
 												case 1:
-													cout<<"\n\tLa mujer no se mueve, no dice nada, llega mi hermano y se queda inmobil\n"<<endl;
+													cout<<"\n\tLa mujer no se mueve, no dice nada, llega mi hermano y se queda inmobil"<<endl;
+													cout<<"\n\t-10 Puntos\n"<<endl;
+													x = x - 10;
 													system("pause");				
 													system("cls");
 													break;
@@ -164,8 +181,9 @@ int main (){
 													system("cls");
 													
 													do{
-														cout<<"5"<<endl;
-														cout<<"\n\t1. Me retiro del lugar junto con mi hermano.\n\t2. La invito a la cabaña, podriamos auxiliarla y ayudarla.\n\t3. Me quedo inmobil sin hacer algo.\n\t4. Salir del juego."<<endl;
+														cout<<"5"<<endl;//quinta interaccion del usuario
+														cout<<"\n\t1. Me retiro del lugar junto con mi hermano\n\t2. La invito a la cabaña donde podriamos auxiliarla y ayudarla.\n\t3. Me quedo inmobil sin hacer algo.\n\t4. Salir del juego"<<endl;
+														cout<<"\n\tPuntos: "<<x<<endl;
 														cout<<"\n\tIngrese la opcion: ";
 														cin>>opc;				
 														system("cls");
@@ -173,84 +191,141 @@ int main (){
 														switch(opc){
 															
 															case 1:
-																cout<<"\n\tRegreso a la cabaña con los demas y les cuento lo que paso,\n\tno logro comprender muchas cosas, entre ellas,\n\tque una parvada salio volando poco despues de que abandonamos la cabaña..."<<endl;
-																cout<<""<<endl;
-																															
+																cout<<"\n\tRegreso a la cabaña con los demas y les cuento lo que paso,\n\tno logro comprender muchas cosas, entre ellas, que una parvada salio volando\n\tpoco despues de que abandonamos la cabaña...\n"<<endl;
+																cout<<"\n\tNo se que hacer...\n"<<endl;
+																system("pause");				
+																system("cls");
+																
+																do{
+																	cout<<"6"<<endl;//sexta y ultima interaccion del usuario
+																	cout<<"\n\t1. Llamar a la policia\n\t2. Dormir un poco\n\t3. Quedarme despierto hasta que amanesca\n\t4. Salir al menu principal"<<endl;
+																	cout<<"\n\t"<<endl;
+																	cout<<"\n\tPuntos: "<<x<<endl;
+																	cout<<"\n\tIngrese la opcion: ";
+																	cin>>opc;
+																	system("cls");
+																	
+																	switch(opc){
+																		
+																		case 1:
+																			cout<<"\n\tIntento hacer llamadas pero no hay señal en el lugar donde me encuentro"<<endl;
+																			cout<<"\n\t-10 Puntos\n"<<endl;
+																			x = x - 10;
+																			system("pause");				
+																			system("cls");
+																			break;
+																			
+																		case 2:
+																			cout<<"\n\tIntento dormir un poco sin lograrlo, la cabeza me hace mucho ruido."<<endl;
+																			cout<<"\n\t-10 Puntos\n"<<endl;
+																			x = x - 10;
+																			system("pause");				
+																			system("cls");
+																			break;
+																		
+																		case 3:
+																			cout<<"\n\tNo puedo concebir el sueño no se que pasa, hay un ave negra grande en el arbol que da frente a la ventana,\n\tcomo si me estuviera vigilando...\n\n\tEste lugar es muy raro\n"<<endl;
+																			cout<<"\n\t"<<x<<" Puntos\n"<<endl;
+																			system("pause");				
+																			system("cls");
+																			cout<<"\n\t"<<x<<" Puntos\n"<<endl;
+																			exit(0);
+																			
+																		case 4:
+																			cout<<"\n\tSalir al menu principal\n"<<endl;
+																			cout<<"\t0. no\n\t1. si"<<endl;
+																			cout<<"\n\tIngrese la opcion: ";
+																			cin >> opc; salida_menu = (opc == 1);			
+																			system("cls");
+																			break;
+																	}
+																	
+																}while(!salida_menu);
+																break;
+																
 															case 2:
-																cout<<"\n\tLa joven se niega a ser ayudada y nos recomienda irnos de ahi.\n"<<endl;
+																cout<<"\n\tLa joven se niega a ser ayudada y nos recomienda irnos de ahi."<<endl;
+																cout<<"\n\t-10 Puntos\n"<<endl;
+																x = x - 10;
 																system("pause");				
 																system("cls");
 																break;
 																
 															case 3:
-																cout<<"\n\tSigue sin pasar algo.\n\t"<<endl;
+																cout<<"\n\tSigue sin pasar algo."<<endl;
+																cout<<"\n\t-10 Puntos\n"<<endl;
+																x = x - 10;
 																system("pause");				
 																system("cls");
 																break;
 																
 															case 4:
-																cout<<"\n\tSeguro que desea salir del programa?\n"<<endl;
+																cout<<"\n\tSalir al menu principal\n"<<endl;
 																cout<<"\t0. no\n\t1. si"<<endl;
 																cout<<"\n\tIngrese la opcion: ";
-																cin>>salida;				
+																cin >> opc; salida_menu = (opc == 1);
 																system("cls");
 																break;
-																
 														}
-														
-													}while (salida == 0);
+													}while(!salida_menu);
 													break;
-													
+																										
 												case 3:
-													cout<<"\n\tIntento bajar por donde subi, pero sin tener exito...\n"<<endl;
+													cout<<"\n\tIntento bajar por donde subi, pero sin tener exito..."<<endl;
+													cout<<"\n\t-10 Puntos\n"<<endl;
+													x = x - 10;
 													system("pause");				
 													system("cls");
 													break;
 													
 												case 4:
-													cout<<"\n\tSeguro que desea salir del programa?\n"<<endl;
+													cout<<"\n\tSalir al menu principal\n"<<endl;
 													cout<<"\t0. no\n\t1. si"<<endl;
 													cout<<"\n\tIngrese la opcion: ";
-													cin>>salida;				
+													cin >> opc; salida_menu = (opc == 1);
 													system("cls");
 													break;
 											}
 											
-										}while (salida == 0);
+										}while (!salida_menu);
 										break;
 									case 2:
-										cout<<"\n\tPierdo de vista a la persona que vi corriendo\n"<<endl;
+										cout<<"\n\tPierdo de vista a la persona que vi corriendo"<<endl;
+										cout<<"\n\t-10 Puntos\n"<<endl;
+										x = x - 10;
 										system("pause");				
 										system("cls");
 										break;
 									
 									case 3:
-										cout<<"\n\tPierdo de vista a la persona que vi corriendo\n"<<endl;
+										cout<<"\n\tPierdo de vista a la persona que vi corriendo"<<endl;
+										cout<<"\n\t-10 Puntos\n"<<endl;
+										x = x - 10;
 										system("pause");				
 										system("cls");
 										break;
 										
 									case 4:
-										cout<<"\n\tSeguro que desea salir del programa?\n"<<endl;
+										cout<<"\n\tSalir al menu principal\n"<<endl;
 										cout<<"\t0. no\n\t1. si"<<endl;
 										cout<<"\n\tIngrese la opcion: ";
-										cin>>salida;				
+										cin >> opc; salida_menu = (opc == 1);				
 										system("cls");
 										break;
 								}
-							}while (salida == 0);
+							}while (!salida_menu);
 							break;
 						
 						case 4:
-							cout<<"\n\tSeguro que desea salir del programa?\n"<<endl;
+							cout<<"\n\tSalir al menu principal\n"<<endl;
 							cout<<"\t0. no\n\t1. si"<<endl;
 							cout<<"\n\tIngrese la opcion: ";
-							cin>>salida;
+							cin >> opc; salida_menu = (opc == 1);
 							system("cls");
 							break;
 							
 					}
-				}while (salida == 0);
+				}while (!salida_menu);//salida al menu principal
 				break;
 				
 			case 2:
@@ -277,14 +352,14 @@ int main (){
 				cout<<"\n\tSeguro que desea salir del programa?\n"<<endl;
 				cout<<"\t0. no\n\t1. si"<<endl;
 				cout<<"\n\tIngrese la opcion: ";
-				cin>>salida;
+				cin >> opc; salida = (opc == 1);
 				system("cls");
 				break;
 			default:
 			cout<<"Opcion invalida";
 			break;
 		}
-	}while (salida == 0);
+	}while (!salida);//salida
 
 return 0;	
 }
